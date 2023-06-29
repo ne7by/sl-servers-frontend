c110c36f65af0187338c9bc15d57fd378b0036ed
horyu1234 <horyu1234@naver.com>
2023-06-29 23:28:39 +0900

    ReactDOM.render 를 createRoot 로 변경

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
 import App from './components/App';
 import {Provider} from "react-redux";
 import store from "./store";
@@ -8,13 +8,14 @@ import "./i18n/i18n";
 
 import 'bootswatch/dist/darkly/bootstrap.min.css';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App/>
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
